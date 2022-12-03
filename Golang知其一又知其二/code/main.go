ddca40d45ef28a1fb4b85b89e4e84541e1fe65cb
NutCat <13534917866@163.com>
2022-12-03 22:22:42 +0800

    doc(knowl): update variable and add codes

---
+++ Golang知其一又知其二/code/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"code/variable"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("---------- case1 ----------")
+	variable.Case1()
+
+	fmt.Println("\n---------- case2 ----------")
+	variable.Case2()
+
+	fmt.Println("\n---------- case3 ----------")
+	variable.Case3()
+
+	fmt.Println("\n---------- case4 ----------")
+	variable.Case4()
+}
