ba19cbc1c6fe5e3dff70e63b84e22a411c7b42ee
NutCat <13534917866@163.com>
2023-04-01 23:51:17 +0800

    feat(sort): Sort API demo and test

--- go_all_clear/chapter07_standard_library/code/code_test.go
+++ go_all_clear/chapter07_standard_library/code/code_test.go
@@ -1,6 +1,9 @@
 package code_7
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
 func Test_sortByTypeDemo(t *testing.T) {
 	sortByTypeDemo()
@@ -16,4 +19,43 @@ func Test_searchDemo(t *testing.T) {
 	t.Log(res1)
 	res2 := searchDemo(sliceCase, 5)
 	t.Log(res2)
+}
+
+func Test_reverseIntDemo(t *testing.T) {
+	s := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
+	res := reverseIntDemo(s)
+	t.Log(res)
+}
+
+func Test_sliceDemo(t *testing.T) {
+	res := sliceDemo()
+	t.Log(res)
+}
+
+func Test_stableDemo(t *testing.T) {
+	res := stableDemo()
+	t.Log(res)
+}
+
+func Test_dataInterfaceSortDemo(t *testing.T) {
+	res := dataInterfaceSortDemo()
+	t.Log(res)
+}
+
+func Test_Authors(t *testing.T) {
+	authors := []Author{
+		{
+			Person: Person{Name: "NutCat", Age: 26, Score: 99},
+			Books:  []string{"母猪的产后护理", "公鸡下蛋教程", "配角如何抢走主角的戏", "宫廷玉液酒"},
+		},
+		{
+			Person: Person{Name: "鲁迅", Age: 10000, Score: 100},
+			Books:  []string{"孔乙己", "骆驼祥子"},
+		},
+	}
+
+	a := AuthorSlice(authors)
+	a.SortAuthors()
+	t.Logf("the author slice is sorted?: %v", sort.IsSorted(a))
+	t.Logf("the sorted authors slice: %v", authors)
 }
--- go_all_clear/chapter07_standard_library/code/sort_example.go
+++ go_all_clear/chapter07_standard_library/code/sort_example.go
@@ -3,6 +3,7 @@ package code_7
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
 func sortByTypeDemo() {
@@ -36,4 +37,132 @@ func searchDemo(s []int, x int) string {
 	} else {
 		return fmt.Sprintf("%d is not found, it should be inserted at index %d", x, i)
 	}
+}
+
+func reverseIntDemo(s []int) string {
+	sort.Sort(sort.Reverse(sort.IntSlice(s)))
+	return fmt.Sprintf("%#v", s)
+}
+
+type Person struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+func sliceDemo() string {
+	people := []Person{
+		{"NutCat", 18, 100},
+		{"Wuu", 27, 100},
+		{"Cat", 10, 100},
+		{"BAO", 27, 90},
+		{"Felix", 26, 99},
+		{"AAA", 26, 83},
+		{"Felix", 29, 87},
+	}
+
+	// sort by name
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Name > people[j].Name
+	})
+
+	s1 := fmt.Sprintf("sort by name: %v\n", people)
+
+	// sort by age
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+
+	s2 := fmt.Sprintf("sort by age: %v\n", people)
+
+	// sort by score
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Score > people[j].Score
+	})
+
+	s3 := fmt.Sprintf("sort by score: %v\n", people)
+
+	return strings.Join([]string{s1, s2, s3}, "\n")
+}
+
+func stableDemo() string {
+	people := []Person{
+		{"NutCat", 18, 100},
+		{"Wuu", 27, 100},
+		{"Cat", 10, 100},
+		{"BAO", 27, 90},
+		{"Felix", 26, 99},
+		{"AAA", 26, 83},
+		{"Felix", 29, 87},
+	}
+
+	// sort by name
+	sort.SliceStable(people, func(i, j int) bool {
+		return people[i].Name > people[j].Name
+	})
+
+	s1 := fmt.Sprintf("sort by name: %v\n", people)
+
+	// sort by age
+	sort.SliceStable(people, func(i, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+
+	s2 := fmt.Sprintf("sort by age: %v\n", people)
+
+	// sort by score
+	sort.SliceStable(people, func(i, j int) bool {
+		return people[i].Score > people[j].Score
+	})
+
+	s3 := fmt.Sprintf("sort by score: %v\n", people)
+
+	return strings.Join([]string{s1, s2, s3}, "\n")
+}
+
+func dataInterfaceSortDemo() string {
+	ints := []int{2, 3, 5, 7, 11, 13}
+	strs := []string{"nut", "cat", "hello", "felix", "wuu"}
+
+	// is sort
+	res1 := fmt.Sprintf(
+		"is ints sorted: %v, is strs sorted: %v\n",
+		sort.IsSorted(sort.IntSlice(ints)),
+		sort.IsSorted(sort.StringSlice(strs)),
+	)
+
+	// sort
+	sort.Sort(sort.StringSlice(strs))
+	res2 := fmt.Sprintf("Sorted string slice :%v\n", strs)
+
+	// stable
+	float64s := []float64{3.14, 2.99, 8.76, 0.98, 2.99, 1.00, 5.55}
+	sort.Stable(sort.Float64Slice(float64s))
+	res3 := fmt.Sprintf("Sorted float64 slice :%v\n", float64s)
+
+	return strings.Join([]string{res1, res2, res3}, "\n")
+}
+
+type Author struct {
+	Person
+	Books []string
+}
+
+type AuthorSlice []Author
+
+func (a AuthorSlice) Len() int {
+	return len(a)
+}
+
+func (a AuthorSlice) Less(i, j int) bool {
+	// return a[i].Age < a[j].Age  // sort by age
+	return len(a[i].Books) > len(a[j].Books)
+}
+
+func (a AuthorSlice) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a AuthorSlice) SortAuthors() {
+	sort.Sort(AuthorSlice(a))
 }
